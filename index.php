40779e7fd8594689522ef1269f2f5c50d4bf809c
HerculanoDev05 <herculanodev05@gmail.com>
2026-04-16 14:41:47 -0300

    feat: add index para deploy no EasyPane

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $logout   = !empty($_GET['logout']);
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
+  <link rel="SHORTCUT ICON" href="imagens/Logo.ico">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login — <?= APP_NAME ?></title>
   <link rel="stylesheet" href="assets/style.css">
